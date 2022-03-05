8b6600d21e6671a0891266ea94a178e832601c1c
lucemouche <101023427+lucemouche@users.noreply.github.com>
2022-03-05 23:36:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import pandas as pd
 import os 
 import matplotlib
 import matplotlib.pyplot as plt
-import seaborn as sns
 from Movie_Database_Build import search_poster
 import fonctions as fct
 
